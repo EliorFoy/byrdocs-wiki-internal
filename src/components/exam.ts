73f28063b13d578fc05b29f371c97bda2c264906
cppHusky <cpphusky@outlook.com>
2026-04-14 18:12:45 +0800

    add component Audio

--- src/components/exam.ts
+++ src/components/exam.ts
@@ -1,3 +1,4 @@
+import Audio from "./exam/Audio.astro";
 import Blank from "./exam/Blank.astro";
 import Choices from "./exam/Choices.astro";
 import Figure from "./exam/Figure.astro";
@@ -5,4 +6,4 @@ import Option from "./exam/Option.astro";
 import Slot from "./exam/Slot.astro";
 import Solution from "./exam/Solution.astro";
 
-export {Blank,Choices,Figure,Option,Slot,Solution};
+export {Audio,Blank,Choices,Figure,Option,Slot,Solution};
